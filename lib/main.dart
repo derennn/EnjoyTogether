e8cb17d10f595c48d3017b80358405b8ca945cc3
derennn <derenbayar78@gmail.com>
2022-11-28 00:23:34 +0300

    frontend improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'fixturepage.dart';
 import 'profilepage.dart';
 import 'themecolors.dart';
 
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Palette.appSwatch,
         fontFamily: 'Roboto',
@@ -21,184 +25,70 @@ class MyApp extends StatelessWidget {
           bodySmall: TextStyle(fontSize: 14.0, color: Colors.white),
         ),
       ),
-      home: const MyHomePage(title: 'Agalarla Maç v.0.0.1'),
+      home: BottomNavBar(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class BottomNavBar extends StatelessWidget {
+  BottomNavBar({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int index = 0;
+  PersistentTabController _controller = PersistentTabController(initialIndex: 0);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text(widget.title)),
-        backgroundColor: Palette.appSwatch.shade100,
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Palette.appSwatch.shade700, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset: true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows: true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        colorBehindNavBar: Palette.appSwatch.shade600,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: Palette.appSwatch.shade800,
-        unselectedItemColor: Colors.white,
-        selectedItemColor: neonGreen,
-        selectedIconTheme: IconThemeData(size: 32),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.scoreboard),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.apps_rounded),
-            label: 'icon2',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Text',
-          )
-        ],
-        onTap: (value) {
-          setState(() {
-            index = value;
-          });
-          if (value == 2) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return ProfilePage();
-                },
-              ),
-            );
-          }
-        },
-        currentIndex: index,
-        elevation: 20,
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties( // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
       ),
-      backgroundColor: Palette.appSwatch.shade200,
-      body: FixturePage(),
+      screenTransitionAnimation: const ScreenTransitionAnimation( // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle: NavBarStyle.style5, // Choose the nav bar style with this property.
     );
   }
 }
 
-class FixturePage extends StatelessWidget {
-  const FixturePage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: ListView.separated(
-              itemCount: 12,
-              itemBuilder: (context, index) {
-                return FixtureBlock();
-              },
-              separatorBuilder: (context, index) => SizedBox(
-                height: 8,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+List<Widget> _buildScreens() {
+  return [
+    FixturePage(),
+    ProfilePage()
+  ];
 }
 
-class FixtureBlock extends StatelessWidget {
-  const FixtureBlock({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(18),
-        ),
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Palette.appSwatch.shade300,
-            Palette.appSwatch.shade500,
-          ],
-        ),
-      ),
-      child: Column(
-        children: [
-          SizedBox(height: 10),
-          Text(
-            'Super Lig',
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                'Galatasaray',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              Text(
-                '-',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              Text(
-                'Fenerbahçe',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          Text(
-            '19.00',
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)))),
-                onPressed: () {},
-                child: Text('Açık Eventler',
-                    style: TextStyle(color: Color(0xff1be2c7))),
-              ),
-              SizedBox(width: 20),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      backgroundColor: Color(0xff1be2c7)),
-                  onPressed: () {},
-                  child: Text(
-                    'Event Aç >',
-                    style: TextStyle(color: Palette.appSwatch.shade300),
-                  )),
-            ],
-          ),
-          SizedBox(height: 10),
-        ],
-      ),
-    );
-  }
+List<PersistentBottomNavBarItem> _navBarsItems() {
+  return [
+    PersistentBottomNavBarItem(
+      icon: Icon(Icons.home),
+      inactiveIcon: Icon(Icons.home_outlined),
+      title: ("Home"),
+      activeColorPrimary: CupertinoColors.activeBlue,
+      inactiveColorPrimary: CupertinoColors.systemGrey,
+    ),
+    PersistentBottomNavBarItem(
+      icon: Icon(Icons.person),
+      inactiveIcon: Icon(Icons.person_outline),
+      title: ("Settings"),
+      activeColorPrimary: CupertinoColors.activeBlue,
+      inactiveColorPrimary: CupertinoColors.systemGrey,
+    ),
+  ];
 }
--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'fixturepage.dart';
+import 'themecolors.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({
@@ -10,12 +12,116 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  bool whichPage = true;
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text('Profile Page', style: TextStyle(color: Colors.black),),
-      ),
-    );
+        backgroundColor: Palette.appSwatch.shade200,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.person_add_alt_outlined,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                  ),
+                ],
+              ),
+              const InkWell(
+                child: CircleAvatar(
+                  radius: 60,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.65,
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text('DB'),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.03,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        whichPage = !whichPage;
+                      });
+                    },
+                    child: Text(
+                      'Eventlerim',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        whichPage = !whichPage;
+                      });
+                    },
+                    child: Text(
+                      'Arkadaşlarım',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+              Expanded(
+                  child: whichPage == true
+                      ? ListView.separated(
+                          itemCount: 8,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text('Aktivite X',
+                                  style: Theme.of(context).textTheme.bodySmall),
+                              trailing: Icon(Icons.add, color: Colors.white),
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 10),
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return Divider(
+                              color: Colors.grey,
+                            );
+                          },
+                        )
+                      : ListView.separated(
+                          itemCount: 5,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text('Ecem',
+                                  style: Theme.of(context).textTheme.bodySmall),
+                              trailing: Icon(Icons.add, color: Colors.white),
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 10),
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return Divider(
+                              color: Colors.grey,
+                            );
+                          },
+                        )),
+            ],
+          ),
+        ));
   }
 }
