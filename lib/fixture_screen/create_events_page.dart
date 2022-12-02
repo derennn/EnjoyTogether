2da1ad17faf36d178a91ce6deda8d7ce97419266
derennn <derenbayar78@gmail.com>
2022-12-02 12:17:15 +0300

    createEventPage improvements

--- lib/fixture_screen/create_events_page.dart
+++ lib/fixture_screen/create_events_page.dart
@@ -48,10 +48,12 @@ class CreateEventPage extends ConsumerStatefulWidget {
   _CreateEventPageState createState() => _CreateEventPageState();
 }
 
-class _CreateEventPageState extends ConsumerState<CreateEventPage> {
+enum radio1 { evet, hayir }
 
-  final Map<String, dynamic> girilen = {};
+class _CreateEventPageState extends ConsumerState<CreateEventPage> {
+  final Map<String, dynamic> values = {};
   final _formKey = GlobalKey<FormState>();
+  radio1? _radioSecilen;
 
   @override
   Widget build(BuildContext context) {
@@ -77,138 +79,202 @@ class _CreateEventPageState extends ConsumerState<CreateEventPage> {
                 children: [
                   const SizedBox(height: 40),
                   Center(
-                    child: Text('Event Aç',
+                    child: RichText(
+                      text: TextSpan(
+                        children: const <TextSpan>[
+                          TextSpan(text: 'Galatasaray - Fenerbahçe'),
+                          TextSpan(text: '\n21.45\n19 Kasım', style: TextStyle(color: Colors.white, fontSize: 17, fontWeight: FontWeight.w400)),
+                        ],
                         style: TextStyle(
-                            fontSize: 26,
-                            color: neonGreen,
-                            fontWeight: FontWeight.w500)),
+                              fontSize: 26,
+                              color: neonGreen,
+                              fontWeight: FontWeight.w500,
+                              height: 1.5,
+                        ),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                   const SizedBox(height: 20),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-              child: Form(
-                key: _formKey,
-                child: SizedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('Galatasaray - Fenerbahçe'),
-                      Text('19 Kasım'),
-                      Text('21.45'),
-                      TextFormField(
-                        style: Theme.of(context).textTheme.bodyMedium,
-                        decoration: const InputDecoration(
-                          hintText: 'Event Nerede Olacak?'
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          style: Theme.of(context).textTheme.bodyMedium,
+                          decoration: const InputDecoration(
+                              hintText: 'Event Nerede Olacak?'),
+                          validator: (value) {
+                            if (value?.isEmpty == true) {
+                              return 'Lokasyon neresi?';
+                            }
+                          },
+                          onSaved: (newValue) {
+                            values['lokasyon'] = newValue;
+                          },
                         ),
-                        validator: (value) {
-                          if (value?.isNotEmpty == true) {
-                            return 'Lokasyon neresi?';
-                          }
-                        },
-                        onSaved: (newValue) {
-                          girilen['lokasyon'] = newValue;
-                        },
-                      ),
-                      const SizedBox(height: 20,),
-                      DropdownButtonFormField(
-                        hint: Text('Hangi Arkadaş Grubu ile Paylaşılsın?',
-                        style: const TextStyle(color: Colors.grey, fontSize: 15),
+                        const SizedBox(
+                          height: 20,
                         ),
-                        decoration: InputDecoration(
-                          border: Theme.of(context).inputDecorationTheme.border,
-                          enabledBorder: Theme.of(context).inputDecorationTheme.enabledBorder,
-                          focusedBorder: Theme.of(context).inputDecorationTheme.focusedBorder,
-                          hintText: 'Event nerede olacak?',
-                        ),
-                        dropdownColor: Palette.appSwatch.shade300,
-                        items: [
-                          DropdownMenuItem(
-                            value: 'Arkadaş grubu 1',
-                              child: Text('Arkadaş grubu 1', style: Theme.of(context).textTheme.bodySmall),
+                        DropdownButtonFormField(
+                          hint: const Text(
+                            'Hangi Arkadaş Grubu ile Paylaşılsın?',
+                            style:
+                                const TextStyle(color: Colors.grey, fontSize: 15),
                           ),
-                          DropdownMenuItem(
-                            value: 'Arkadaş grubu 2',
-                              child: Text('Arkadaş grubu 2', style: Theme.of(context).textTheme.bodySmall),
+                          decoration: InputDecoration(
+                            border: Theme.of(context).inputDecorationTheme.border,
+                            enabledBorder: Theme.of(context)
+                                .inputDecorationTheme
+                                .enabledBorder,
+                            focusedBorder: Theme.of(context)
+                                .inputDecorationTheme
+                                .focusedBorder,
+                            hintText: 'Event nerede olacak?',
                           ),
-                        ],
-                        value: girilen['arkadasgrubu'],
-                        onChanged: (value) {
-
-                        },
-                        validator: (value) {
-                          return 'Hangi arkadaş grubuyla paylaşılsın?';
-                        },
-                        onSaved: (newValue) {
-                          setState(() {
-                            girilen['arkadasgrubu'] = newValue;
-                          });
-                        },
-                      ),
-                      FormField(
+                          dropdownColor: Palette.appSwatch.shade300,
+                          items: [
+                            DropdownMenuItem(
+                              value: 'Arkadaş grubu 1',
+                              child: Text('Arkadaş grubu 1',
+                                  style: Theme.of(context).textTheme.bodySmall),
+                            ),
+                            DropdownMenuItem(
+                              value: 'Arkadaş grubu 2',
+                              child: Text('Arkadaş grubu 2',
+                                  style: Theme.of(context).textTheme.bodySmall),
+                            ),
+                          ],
+                          value: values['arkadasGrubu'],
+                          onChanged: (value) {},
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Bir arkadaş grubu seç';
+                            }
+                          },
+                          onSaved: (newValue) {
+                            setState(() {
+                              values['arkadasGrubu'] = newValue;
+                            });
+                          },
+                        ),
+                        FormField(
+                          initialValue: _radioSecilen,
                           builder: (field) {
                             return Padding(
-                              padding: EdgeInsets.all(10),
+                              padding: EdgeInsets.only(top: 14, left: 14),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text('Deneme 1 2 3'),
-                                  field.hasError
-                                  ? Text(
-                                    field.errorText!,
-                                    style: TextStyle(color: Colors.red)
-                                  )
-                                  : Container(),
-                                  RadioListTile(
-                                      value: 'value',
-                                      groupValue: 'group value',
-                                      onChanged: (value) {
-                                        value = value;
-                                      },
+                                  Text('Arkadaş getirmek serbest mi?',
+                                  style: Theme.of(context).textTheme.bodySmall,
                                   ),
-
+                                  AnimatedContainer(
+                                    curve: Curves.ease,
+                                        duration: Duration(milliseconds: 300),
+                                        height: field.hasError ? 35 : 0,
+                                        child: AnimatedCrossFade(
+                                          firstChild: Align(
+                                            alignment: Alignment.centerLeft,
+                                            child: Text('${field.errorText}',
+                                                style: TextStyle(color: Colors.red, fontSize: Theme.of(context).textTheme.bodySmall!.fontSize)),
+                                          ),
+                                          secondChild: const SizedBox(),
+                                          crossFadeState: field.hasError ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+                                          duration: Duration(milliseconds: 300),
+                                        ),
+                                      ),
+                                  ListTile(
+                                    contentPadding: EdgeInsets.zero,
+                                    dense: true,
+                                    title: Text('Evet',
+                                    style: Theme.of(context).textTheme.bodySmall,
+                                    ),
+                                    leading: Radio<radio1>(
+                                      fillColor: MaterialStateColor.resolveWith((states) => neonGreen),
+                                      value: radio1.evet,
+                                      groupValue: _radioSecilen,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          _radioSecilen = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  ListTile(
+                                    contentPadding: EdgeInsets.zero,
+                                    dense: true,
+                                    title: Text('Hayır',
+                                      style: Theme.of(context).textTheme.bodySmall,
+                                    ),
+                                    leading: Radio<radio1>(
+                                      fillColor: MaterialStateColor.resolveWith((states) => neonGreen),
+                                      value: radio1.hayir,
+                                      groupValue: _radioSecilen,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          _radioSecilen = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
                                 ],
                               ),
                             );
                           },
-                        validator: (value) {
-                          if (value != true ) {
-                            return 'please choose sth';
-                          }
-                          return null;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Material(
-              color: Palette.appSwatch.shade500,
-              child: Hero(
-                tag: widget.index.toString(),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(12))),
-                        backgroundColor: Color(0xff1be2c7)),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        'Arkadaşlarınla Paylaş',
-                        style: TextStyle(
-                            color: Palette.appSwatch.shade300, fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Lütfen birini seçin';
+                            }
+                          },
+                          onSaved: (newValue) {
+                            setState(() {
+                              values['arkadasGetirmekSerbestMi'] = newValue;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 10),
+                        Material(
+                          color: Palette.appSwatch.shade500,
+                          child: Hero(
+                            tag: widget.index.toString(),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  shape: const RoundedRectangleBorder(
+                                      borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                                  backgroundColor: Color(0xff1be2c7)),
+                              onPressed: () {
+                                final formState = _formKey.currentState;
+                                if (formState == null) return print('formState == null');
+                                if (formState.validate() == true) {
+                                  formState.save();
+                                  saveFunc(values);
+                                }
+                              },
+                              child: SizedBox(
+                                width: double.infinity,
+                                child: Text(
+                                  'Event Aç',
+                                  style: TextStyle(
+                                      color: Palette.appSwatch.shade300, fontSize: 18),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -219,4 +285,16 @@ class _CreateEventPageState extends ConsumerState<CreateEventPage> {
       ),
     );
   }
+
+  void saveFunc (value) {
+    Navigator.pop(context);
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(
+                'Event açıldı! Lokasyon: ${values['lokasyon']}, Arkadaş grubu: ${values['arkadasGrubu']}, Arkadaş getirmek serbest: ${values['arkadasGetirmekSerbestMi'] == radio1.evet ? 'Evet' : 'Hayır'}',
+            ),
+          backgroundColor: Palette.appSwatch.shade700,
+        ),
+    );
+    }
 }
